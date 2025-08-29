117bd5af65536bf799525a33051b331a7b06b3b1
Paulo Martins <pjfmartins83@gmail.com>
2025-08-29 14:26:26 -0300

    29/08/2025

--- genres/views.py
+++ genres/views.py
@@ -31,3 +31,10 @@ def genre_detail_view(request, pk):
         genre.name = data["name"]
         genre.save()
         return JsonResponse({"id": genre.id, "name": genre.name})
+
+    elif request.method == "DELETE":
+        genre.delete()
+        return JsonResponse(
+            {"message": "Gênero excluído com sucesso"},
+            status=204,
+        )
