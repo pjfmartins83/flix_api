8fedccf5aab86082ec1f31f920bed200558dc061
Paulo Martins <pjfmartins83@gmail.com>
2025-09-08 10:59:33 -0300

    08/09/2025

--- genres/views.py
+++ genres/views.py
@@ -1,16 +1,17 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from core.permissions import GlobalPermission
 from genres.models import Genre
 from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalPermission)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalPermission)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,17 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from core.permissions import GlobalPermission
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalPermission)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalPermission)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
